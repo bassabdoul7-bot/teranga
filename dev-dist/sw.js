a06f454a1003b56fab99a9289e4c4b6aeda5d641
Abdoul Bass <bassabdoul7@gmail.com>
2025-11-03 21:20:49 -0500

    fix: Correct netlify.toml syntax

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n0fg0ugseq8"
+    "revision": "0.2kc97kuqvr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
